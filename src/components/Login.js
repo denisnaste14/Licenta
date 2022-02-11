c0c021251d102a3d0e7c2973b2cf14d83e2a1220
Denis <denisnaste@yahoo.com>
2022-02-11 21:40:23 +0200

    login start

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './StylesCss/Login.css'
+function Login() {
+  return (
+      <>
+        <div className='login'>
+            <div className='login-container'>
+                <div className='login-title'>
+                    Log In
+                </div>
+            </div>
+        </div>
+      </>
+  )
+}
+
+export default Login
---
+++ src/components/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Signup() {
+  return (
+      <>
+        <div className='signup'>
+            <div className='signup-container'>
+                
+            </div>
+        </div>
+        
+      </>
+  )
+}
